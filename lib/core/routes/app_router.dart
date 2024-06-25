13ded5d888e81080c8d3fcdc03076c3a1941eb45
mohammedmosaad <mohammedmosaad869@gmail.com>
2024-06-26 00:54:12 +0300

    add firebase to our project

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,6 +1,9 @@
+import 'package:dalil/core/services/service_locator.dart';
+import 'package:dalil/features/auth/features/auth_cubit/cubit/auth_cubit.dart';
 import 'package:dalil/features/auth/features/views/sign_up_view.dart';
 import 'package:dalil/features/on_boarding/presentation/views/on_boarding_view.dart';
 import 'package:dalil/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../features/auth/features/views/sign_in_view.dart';
@@ -16,10 +19,16 @@ final GoRouter router = GoRouter(routes: [
   ),
   GoRoute(
     path: '/signUp',
-    builder: (context, state) => const SignUpView(),
+    builder: (context, state) => BlocProvider(
+      create: (context) => getIt<AuthCubit>(),
+      child: const SignUpView(),
+    ),
   ),
   GoRoute(
     path: '/signIn',
-    builder: (context, state) => const SignInView(),
+    builder: (context, state) => BlocProvider(
+      create: (context) => getIt<AuthCubit>(),
+      child: const SignInView(),
+    ),
   ),
 ]);
